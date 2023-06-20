03480907eb22d26ec14546621cea34c8b4110b69
Name <sirotenkostepan@mail.ru>
2023-06-20 22:40:04 +0700

    start

---
+++ app/src/main/java/com/example/saper/GameSettings.kt
@@ -0,0 +1,11 @@
+package com.example.saper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameSettings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_settings)
+    }
+}
--- app/src/main/java/com/example/saper/MainActivity.kt
+++ app/src/main/java/com/example/saper/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.saper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.saper.databinding.ActivityGameSettingsBinding
+import com.example.saper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var vb : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        vb = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(vb.root)
+
+        //Надо будет реализовать проверку на наличие прогресса...
+        //Пока что можно только начать новую игру
+
+        vb.btNewGame.setOnClickListener {
+            intent = Intent(this, GameSettings::class.java)
+            startActivity(intent)
+        }
+
+        vb.btResum.setOnClickListener {
+
+        }
     }
 }
